8efdeda9b3428839c7279320801e0c1cc55b3977
nntram <tramb9531@gmail.com>
2021-12-09 12:55:53 +0700

    Xong tài khoản User

---
+++ TN213_MuaSamCanTho/Content/js/account.js
@@ -0,0 +1,72 @@
+﻿// Show/hide password onClick of button using Javascript only
+
+// https://stackoverflow.com/questions/31224651/show-hide-password-onclick-of-button-using-javascript-only
+$(document).ready(function () {
+    function show() {
+        var p = document.getElementById('pwd');
+        p.setAttribute('type', 'text');
+    }
+
+    function hide() {
+        var p = document.getElementById('pwd');
+        p.setAttribute('type', 'password');
+    }
+
+    var pwShown = 0;
+    console.log("eye= " + document.getElementById("eye"))
+    document.getElementById("eye").addEventListener("click", function () {
+        if (pwShown == 0) {
+            pwShown = 1;
+            show();
+        } else {
+            pwShown = 0;
+            hide();
+        }
+    }, false);
+
+
+
+    function show2() {
+        var p = document.getElementById('pwd2');
+        p.setAttribute('type', 'text');
+    }
+
+    function hide2() {
+        var p = document.getElementById('pwd2');
+        p.setAttribute('type', 'password');
+    }
+
+    var pwShown2 = 0;
+    console.log("eye2= " + document.getElementById("eye2"))
+    document.getElementById("eye2").addEventListener("click", function () {
+        if (pwShown2 == 0) {
+            pwShown2 = 1;
+            show2();
+        } else {
+            pwShown2 = 0;
+            hide2();
+        }
+    }, false);
+
+
+
+    function checkPasswordMatch() {
+        var password = $("#pwd").val();
+        var confirmPassword = $("#pwd2").val();
+
+        if (password != confirmPassword)
+            $("#checkPasswordMatch").html("Passwords does not match!").css("color", "red");
+        else
+            $("#checkPasswordMatch").html("Passwords match.").css("color", "green");
+    }
+
+    $("#pwd").keyup(checkPasswordMatch);
+    $("#pwd2").keyup(checkPasswordMatch);
+
+    $("#myform").submit(function () {
+        var pwdMes = $("#checkPasswordMatch").html();
+        var notMatch = pwdMes != "Passwords match." && pwdMes != "";
+        if (notMatch) return false;
+      
+    })
+})
--- TN213_MuaSamCanTho/Controllers/UserController.cs
+++ TN213_MuaSamCanTho/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CaptchaMvc.HtmlHelpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -68,35 +69,82 @@ namespace TN213_MuaSamCanTho.Controllers
         [HttpPost]
         public ActionResult DangKy(FormCollection f)
         {
-            if (ModelState.IsValid)
+            //Kiểm tra captcha hợp lệ
+            if (!this.IsCaptchaValid("Captcha is not valid!"))
+                TempData["error"] = "Mã captcha không đúng!";
+            else
             {
-                string sname = f["username"].ToString();
-                string spass = Tools.Assistance.MaHoaMatKhau(f["password"]);
-                Console.WriteLine(spass);
-                TaiKhoan kh = db.TaiKhoans.SingleOrDefault(n => n.TenDangNhap == sname && n.MatKhau == spass);
-
-                //nếu thành công
-                if (kh != null)
+                if (ModelState.IsValid)
                 {
-                    kh.MatKhau = ""; //Không lưu mật khẩu để bảo mật
-                    Session["TaiKhoan"] = kh;
-                    return RedirectToAction("Index", "Home");
+                    var username = f["username"];
+                    var kttk = db.TaiKhoans.FirstOrDefault(n => n.TenDangNhap == username);
+                    
+                    if (kttk == null)
+                    {
+                        TaiKhoan tk = new TaiKhoan();
+                        tk.TenNguoiDung = f["name"];
+                        tk.TenDangNhap = f["username"];
+                        tk.MatKhau = Tools.Assistance.MaHoaMatKhau(f["password"]);
+                        tk.QuyenAdmin = false;
+
+                        db.TaiKhoans.Add(tk); //lưu vào dataset
+                        db.SaveChanges();//thêm vào csdl
+
+                        TempData["success"] = "Đăng ký thành công! Vui lòng đăng nhập để kiểm tra.";
+                        return RedirectToAction("DangNhap");
+                    }
+                    else 
+                        TempData["error"] = "Tên tài khoản đã tồn tại! Vui lòng chọn tên tài khoản khác.";
+                   
                 }
                 else
-                {
-                    TempData["error"] = "Sai tên đăng nhập hoặc mật khẩu!";
-                    return View();
-                }
+                    TempData["error"] = "Đã có lỗi xảy ra.";
+
             }
 
-            //nếu không thành công
-            TempData["error"] = "Lối xảy ra khi đăng nhập!";
             return View();
-
         }
 
-        public ActionResult test()
+        [HttpGet]
+        public ActionResult SuaThongTin()
         {
+            if (Session["TaiKhoan"] != null)
+            {
+                TaiKhoan tk = Session["TaiKhoan"] as TaiKhoan;
+                return View(tk);
+            }
+            //Nếu chưa đăng nhập thì đưa về trang chủ
+            return RedirectToAction("Index", "Home");
+        }
+
+
+
+        [HttpPost]
+        public ActionResult SuaThongTin(FormCollection f)
+        {
+                if (ModelState.IsValid)
+                {
+                    int id = int.Parse(f["id"]);
+                    var tk = db.TaiKhoans.FirstOrDefault(n => n.MaNguoiDung == id);
+
+                    if (tk != null)
+                    {
+                        tk.TenNguoiDung = f["name"];
+                        if(f["password"] != "")
+                            tk.MatKhau = Tools.Assistance.MaHoaMatKhau(f["password"]);
+
+                        db.SaveChanges();//thêm vào csdl
+                        TempData["success"] = "Lưu thay đổi thành công!";
+                        Session["TaiKhoan"] = tk;
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                        TempData["error"] = "Tài khoản không tồn tại.";
+
+                }
+                else
+                    TempData["error"] = "Đã có lỗi xảy ra.";
+
             return View();
         }
     }
